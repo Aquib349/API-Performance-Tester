021ac77a9b32b983b0098b9900a5641e1c1b849d
Aquib349 <aquib_m@optimusbt.net>
2024-05-16 11:02:34 +0530

    new change

--- performance-script.js
+++ performance-script.js
@@ -3,8 +3,8 @@ const express = require("express");
 const cors = require("cors");
 const app = express();
 
-app.use(express.json());
 app.use(cors());
+app.use(express.json());
 
 app.post("/api/performance", async (req, res) => {
   const { urls, websocketURL } = req.body;
