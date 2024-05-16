5b400d285b8542aecce11e6cc942dade379ed1a9
Aquib349 <aquib_m@optimusbt.net>
2024-05-16 11:13:41 +0530

    new changes

--- performance-script.js
+++ performance-script.js
@@ -6,6 +6,14 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+// Enable CORS for all requests
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "http://localhost:5173");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  next();
+});
+
 app.post("/api/performance", async (req, res) => {
   const { urls, websocketURL } = req.body;
   const performanceData = [];
